78fcaa409f20192b33e920b3e79bdc32336d04bc
dman1991g <141966927+dman1991g@users.noreply.github.com>
2023-10-04 15:09:31 -0400

    Update script.js
    Added a new quote

--- script.js
+++ script.js
@@ -26,7 +26,10 @@ const quotes = [
   {
     quote: "The only true wisdom is in knowing you know nothing.",
     author: "— Socrates"
-  }
+  },
+  {
+    quote: "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
+    author: "- Thomas Edison"
 ]
 
 button.addEventListener("click", function() {
