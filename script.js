490a17f80c9c814b18df037d6c242236424f0eb6
dman1991g <141966927+dman1991g@users.noreply.github.com>
2023-10-04 15:18:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const quotes = [
     author: "— Theodore Roosevelt"
   },
   {
-    quote: "I have not failed. I've just found 10,000 ways that won't work.",
+    quote: "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
     author: "— Thomas Edison"
   },
   {
